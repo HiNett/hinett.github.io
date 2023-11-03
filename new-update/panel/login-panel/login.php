3e41f3728086a7ab5816b6748d7af8ca9e985320
HiNett <elone.delille@algosup.com>
2023-11-03 22:21:07 +0100

    Creating Basic Files
    Creating Basic Files for the GitHub Update Project

---
+++ new-update/panel/login-panel/login.php
@@ -0,0 +1,17 @@
+<?php
+//Coming soon...
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coming soon...</title>
+</head>
+<body>
+    Coming soon...
+</body>
+<footer>
+    Copyright 2023 - DELILLE Elone
+</footer>
+</html>
